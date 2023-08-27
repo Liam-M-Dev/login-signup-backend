bb73966f59948442b7f37a788c556fc00f1ba923
Liam-M-Dev <liammassey21@gmail.com>
2023-08-27 15:47:57 +1000

    Build custom field and email middleware

--- src/middleware/UserMiddleware.js
+++ src/middleware/UserMiddleware.js
@@ -2,3 +2,26 @@ const { UserModel } = require("../models/User");
 
 const { validateHashData } = require("../services/AuthServices");
 
+const checkUserFields = async (request, response, next) => {
+    const {email, password} = request.body;
+
+    if (!email || !password){
+        let error = new Error({message: "Missing password or email"});
+        error.statusCode = 400
+        next(error)
+    }
+
+    next()
+
+}
+
+const checkValidEmail = async (request, response, next) => {
+    const savedUser = await UserModel.findOne({email: request.body.email});
+    if (!savedUser){
+        let error = new Error({message: "Incorrect email, try again"});
+        error.statusCode = 400
+        next(error)
+    }
+    
+    next();
+}
