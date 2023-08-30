219ddf45496b60647235d9eaa09c94c7599b5007
Liam-M-Dev <liammassey21@gmail.com>
2023-08-30 10:38:07 +1000

    Modify login middleware, add error handler middleware

--- src/middleware/UserMiddleware.js
+++ src/middleware/UserMiddleware.js
@@ -2,26 +2,42 @@ const { UserModel } = require("../models/User");
 
 const { validateHashData } = require("../services/AuthServices");
 
-const checkUserFields = async (request, response, next) => {
-    const {email, password} = request.body;
+// Middleware to confirm fields for sign-up meet requirements (work in progress)
+// const checkUserFields = async (request, response, next) => {
+//     const {email, password} = request.body;
 
-    if (!email || !password){
-        let error = new Error({message: "Missing password or email"});
-        error.statusCode = 400
-        next(error)
-    }
+//     if (!email || !password){
+//         let error = new Error({message: "Missing password or email"});
+//         error.statusCode = 400
+//         next(error)
+//     }
 
-    next()
+//     if (email)
 
-}
+//     next()
 
-const checkValidEmail = async (request, response, next) => {
+// }
+
+// Login specific middleware,
+// Checks request body email matches with user email within database
+// Checks request body password matches with stored password, 
+// using bcrypt compare method
+// If all tests pass, middleware is complete and continues to next step in chain
+// If a check fails then error is passed through to error handling middleware.
+const loginMiddleware = async (request, response, next) => {
     const savedUser = await UserModel.findOne({email: request.body.email});
     if (!savedUser){
         let error = new Error({message: "Incorrect email, try again"});
-        error.statusCode = 400
-        next(error)
+        error.statusCode = 400;
+        next(error);
+    } else {
+        let validPassword = await validateHashData(request.body.password, savedUser.password);
+        if (!validPassword){
+            let error = new Error({message: "Invalid Password"});
+            error.statusCode = 400;
+            next(error);
+        }
+        next();
     }
-    
-    next();
 }
+
