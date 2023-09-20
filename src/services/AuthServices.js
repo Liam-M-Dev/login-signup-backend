c709e381ed570f3e2fdb4c596891afc6201c9694
Liam-M-Dev <liammassey21@gmail.com>
2023-09-20 14:54:49 +1000

    Add JWT functionality

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -1,7 +1,19 @@
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
 
 const dotenv = require("dotenv");
 dotenv.config();
+
+// ---- encryption and decryption ----
+
+let encAlgorithm = "aes-256-cbc";
+let encPrivateKey = crypto.scryptSync(process.env.ENC_KEY, "SpecialSalt", 32);
+let encIV = crypto.scryptSync(process.env.ENC_IV, "SpecialSalt", 16);
+let cipher = crypto.createCipheriv(encAlgorithm, encPrivateKey, encIV);
+let decipher = crypto.createDecipheriv(encAlgorithm, encPrivateKey, encIV);
+
+
 
 // Create functions for bcrypt password hashing and verification
 const saltRounds = 10;
